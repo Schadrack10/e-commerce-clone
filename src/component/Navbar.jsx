c29530a00713fd1c38d50f59d0ba43b09bb8cf22
schadrack <schadrackbotombe@gmail.com>
2021-12-07 16:31:06 +0200

    commited changes

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Navbar = () => {
     return (
       <>
-     <nav class="navbar navbar-expand-lg navbar-light bg-danger">
+     <nav class="navbar navbar-expand-lg navbar-light bg-white py-3 shadow-sm">
   <div class="container">
     <a class="navbar-brand fw-bold fs-4" href="#">
       E-Commerce
@@ -38,16 +38,16 @@ const Navbar = () => {
         <button class="btn btn-outline-success" type="submit">Search</button>
       </form> */}
       <div className="buttons">
-          <a href="" className="btn btn-outline-dark">
+          <a href="" className="btn btn-outline-dark ms-2">
             <i className="fa fa-sign-in me-1"></i>
             Login
             </a>  
-          <a href="" className="btn btn-outline-dark">
-            <i className="fa fa-sign-in me-1"></i>
+          <a href="" className="btn btn-outline-dark ms-2">
+            <i className="fa fa-user-plus m-1"></i>
             Register
             </a>  
-          <a href="" className="btn btn-outline-dark">
-            <i className="fa fa-sign-in me-1"></i>
+          <a href="" className="btn btn-outline-dark ms-2">
+            <i className="fa fa-shopping-cart me-1"></i>
             Cart(0)
             </a>  
       </div>
